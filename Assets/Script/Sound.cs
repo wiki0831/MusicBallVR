bdd6d587960cebcf4007c2da93fd96a0cf7967eb
wiki0831 <wiki0831@gmail.com>
2018-10-15 15:12:44 -0400

    Sound and HUD display

---
+++ Assets/Script/Sound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour {
+
+    AudioSource audioSource;
+    public AudioClip drumSound;
+
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        float vol  = collision.relativeVelocity.magnitude * 0.2f;
+        Debug.Log(vol);
+        audioSource.PlayOneShot(drumSound, vol);
+    }
+}
